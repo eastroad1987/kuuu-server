cb9b49a3021d4cc962a5536fdd04a73044dceda3
eastroad <eastroad1987@gmail.com>
2025-04-27 23:06:57 +0900

    test update

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,3 +1,22 @@
-import server from "../src/main";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "../src/app.module";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import express from "express";
 
-export default server;
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, adapter);
+  app.enableCors();
+  await app.init();
+  return expressApp;
+}
+
+let server: any;
+export const handler = async (event: any, context: any) => {
+  if (!server) {
+    server = await bootstrap();
+  }
+  return server(event, context);
+};
